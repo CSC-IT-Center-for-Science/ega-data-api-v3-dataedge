ce85f6691e242e7c8a00b57019bbfeb6c331dc1c
asenf <asenf@asenf-virtual-machine>
2017-04-26 11:04:16 +0100

    Make Swagger UI public. Add Specific HTTP Error Response Codes (GA4GH) First code implementing GA4GH blob server functionality

--- src/main/java/eu/elixir/ega/ebi/dataedge/rest/FileController.java
+++ src/main/java/eu/elixir/ega/ebi/dataedge/rest/FileController.java
@@ -15,18 +15,21 @@
  */
 package eu.elixir.ega.ebi.dataedge.rest;
 
+import eu.elixir.ega.ebi.dataedge.config.InvalidAuthenticationException;
+import eu.elixir.ega.ebi.dataedge.config.UnsupportedFormatException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import org.springframework.web.bind.annotation.RestController;
 import eu.elixir.ega.ebi.dataedge.service.FileService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -63,8 +66,8 @@ public class FileController {
     // Experimental - Return a BAM Header
     @RequestMapping(value = "/{file_id}/header", method = GET)
     public Object getFileHeader(@PathVariable String file_id,
-                              @RequestParam(value = "destinationFormat", required = false, defaultValue="aes128") String destinationFormat,
-                              @RequestParam(value = "destinationKey", required = false, defaultValue = "") String destinationKey) {
+                                @RequestParam(value = "destinationFormat", required = false, defaultValue="aes128") String destinationFormat,
+                                @RequestParam(value = "destinationKey", required = false, defaultValue = "") String destinationKey) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         
         return fileService.getFileHeader(auth, 
@@ -72,6 +75,49 @@ public class FileController {
                                          destinationFormat,
                                          destinationKey);
         
+    }
+    
+    // {id} -- 'file', 'sample', 'run', ...
+    @RequestMapping(value = "/byid/{id}", method = GET)
+    @ResponseBody
+    public void getById(@PathVariable String idType,
+                        @RequestParam(value = "accession", required = true) String accession,
+                        @RequestParam(value = "format", required = false, defaultValue = "bam") String format,
+                        @RequestParam(value = "chr", required = false, defaultValue = "") String reference,
+                        @RequestParam(value = "start", required = false, defaultValue = "0") long start,
+                        @RequestParam(value = "end", required = false, defaultValue = "0") long end, 
+                        @RequestParam(value = "destinationFormat", required = false, defaultValue="aes128") String destinationFormat,
+                        @RequestParam(value = "destinationKey", required = false, defaultValue = "") String destinationKey,
+                        HttpServletRequest request,
+                        HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth==null) {
+            throw new InvalidAuthenticationException(accession);
+        }
+            
+        // Basic Parameter Validation
+        validateFormat(format);
+        
+        fileService.getById(auth,
+                            idType, 
+                            accession, 
+                            format, 
+                            reference,
+                            start, 
+                            end, 
+                            destinationFormat,
+                            destinationKey,
+                            request, 
+                            response);
+    }
+
+    // Ensure BAM/CRAM & General Parameters
+    private void validateFormat(String format) {
+        if  (!((format.equalsIgnoreCase("bam") || format.equalsIgnoreCase("cram")))) {
+            throw new UnsupportedFormatException(format);            
+        }
+        
+        // TODO ... Other Validations according to API Specs
     }
     
     
