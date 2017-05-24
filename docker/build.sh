bf0986f7e4f77bda0cd80d23dd315feee283f923
asenf <asenf@asenf-virtual-machine>
2017-05-24 10:22:43 +0100

    Update

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+git clone https://github.com/elixir-europe/ega-data-api-v3-cipher.git
+mvn -f /ega-data-api-v3-cipher/pom.xml install
 git clone https://github.com/elixir-europe/ega-data-api-v3-dataedge.git
 mvn -f /ega-data-api-v3-dataedge/pom.xml install
 mv /ega-data-api-v3-dataedge/target/DataEdgeService-0.0.1-SNAPSHOT.war /EGA_build
